d0a0d6b179cf992653678abc125921a004041a35
Athos-0day <athos.0day@protonmail.com>
2026-02-25 21:05:48 +0100

    chore: add special character in putchar

--- kernel/console.c
+++ kernel/console.c
@@ -1,14 +1,57 @@
 #include <n7OS/console.h>
+#include <../include/debug.h>
 
+/* Variable to stock the screen adress */
 uint16_t *scr_tab;
+/* Actual position in the screen (cursor) */
+uint16_t pos;
 
 void init_console() {
+    /* Set the pointer of the screen. */
     scr_tab= (uint16_t *) SCREEN_ADDR;
+    /* Initilisation of the position. */
+    pos = 0u;
 }
 
 void console_putchar(const char c) {
-    if (c == 'a') 
-        scr_tab[0]= CHAR_COLOR<<8|c;
+    /* ASCII Character */
+    if ((c > 31u) && (c < 127u)) { 
+        scr_tab[pos]= CHAR_COLOR<<8 | c;
+        pos++;
+    }
+    /* Line Feed */
+    else if (c == '\n') {
+        pos += (VGA_WIDTH - (pos % VGA_WIDTH));
+    }
+    /* Carriage Return */
+    else if (c == '\r') {
+        pos -= (pos % VGA_WIDTH);
+    }
+    /* Horizontal Tab */
+    else if (c == '\t') {
+        for (uint8_t i = 0u; i < 8u; i++) {
+            scr_tab[pos] = CHAR_COLOR<<8 | ' ';
+            pos++;
+        }
+    }
+    /* Backspace */
+    else if (c == '\b') {
+        if (pos > 0u) {
+            pos--;
+        }
+    }
+    /* Form Feed */
+    else if (c == '\f') {
+        for (uint16_t i = 0u; i < (VGA_WIDTH * VGA_HEIGHT); i ++) {
+            scr_tab[i] = CHAR_COLOR<<8 | ' ';
+        }
+        pos = 0u;
+    }
+
+    /* Ensure pos does not overflow */
+    if (pos >= VGA_WIDTH * VGA_HEIGHT) {
+        pos = (VGA_HEIGHT - 1) * VGA_WIDTH;
+    }
 }
 
 void console_putbytes(const char *s, int len) {
