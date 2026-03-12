41980e24e0580a116b1c5554a40306a38aa1e712
Athos-0day <athos.0day@protonmail.com>
2026-03-12 11:42:30 +0100

    handler for interruption 50

--- kernel/test_irq.c
+++ kernel/test_irq.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 
 extern void handler_IT();
+extern void handler_IT50();
 
 
 void init_irq() {
+    init_irq_entry(50,(uint32_t) handler_IT50);
 
 }
 
 void handler_en_C() {
 
+}
+
+void handler_it50() {
+
+    printf("J'ai bien recu l'interruption 50\n");
 }
