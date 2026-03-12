5d6c0c319202574421e593c09e74c8b45b9a4432
Athos-0day <athos.0day@protonmail.com>
2026-03-12 11:42:08 +0100

    add: paging test and irq 50 test

--- kernel/start.c
+++ kernel/start.c
@@ -6,15 +6,27 @@
 #include <n7OS/kheap.h>
 #include <n7OS/mem.h>
 #include <../include/debug.h>
+#include <n7OS/irq.h>
+extern void handler_IT50(void);
 
 void kernel_start(void)
 {
     initialise_paging();
     init_console();
-    print_mem();
 
     // lancement des interruptions
     sti();
+
+    init_irq_entry(50,(uint32_t) handler_IT50);
+    /** 
+    // test de la pagination
+    alloc_page_entry(0xA0000000,1,0); 
+    uint32_t *ptr = (uint32_t *) 0xA0000000;
+    (*ptr)++;
+    */
+
+    // test de l'interruption 50
+    __asm__ __volatile__("int $50"::);
 
     // on ne doit jamais sortir de kernel_start
     while (1) {
